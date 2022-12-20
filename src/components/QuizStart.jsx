89487d5e04d5bcf414c4d6e02312ec223d846bbb
MyselfRoshan <roshanghimire699@gmail.com>
2022-12-20 22:01:41 +0545

    feat: added quiz starting page

---
+++ src/components/QuizStart.jsx
@@ -0,0 +1,9 @@
+export default function QuizStart() {
+  return (
+    <div className="quizzical">
+      <h1>Quizzical</h1>
+      {/* <p></p> */}
+      <button className="start-quiz-btn">Start Quiz</button>
+    </div>
+  );
+}
