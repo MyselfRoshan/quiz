d5081a7fec7fb4da686ef4647fb811cf2336b5d6
MyselfRoshan <roshanghimire699@gmail.com>
2023-02-13 20:44:36 +0545

    feat:added mode selection butttons

---
+++ src/components/ModeSelectionContainer.jsx
@@ -0,0 +1,6 @@
+import ModeSelectionItem from "./ModeSelectionItem";
+function ModeSelectionContainer(props) {
+  console.log(props);
+  return <div className="mode-selection-container">{"hi"}</div>;
+}
+export default ModeSelectionContainer;
