65b87a24d829302f0a1947f8c0881908f061f072
MyselfRoshan <roshanghimire699@gmail.com>
2023-02-17 21:01:42 +0545

    feat: added functionality to chose different options for difficulty, category and type of quiz questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useCallback } from "react";
 import QuizQuestion from "./components/QuizQuestion";
 import QuizStart from "./components/QuizStart";
 
@@ -16,29 +17,56 @@ function App() {
     category: "", //?
     type: "", //?Multiple choice ='multiple' true/false='boolean'
   });
+  console.log(apiUri);
 
-  useEffect(() => {
+  const apiCall = useCallback(() => {
     const apiBaseURL = "https://opentdb.com/api.php?";
     getQuizQuestion();
 
     async function getQuizQuestion() {
       const response = await fetch(
-        `${apiBaseURL}amount=${apiUri.amount}${apiUri.encoding}`,
+        `${apiBaseURL}amount=${apiUri.amount}${apiUri.encoding}${apiUri.difficulty}${apiUri.type}${apiUri.category}`,
       );
       const data = response.ok
         ? await response.json()
         : Promise.reject(response);
       console.log(data);
-      setQuizzesArray(data.results);
-      setOptionArray(
-        data.results.map((quiz) => {
-          const optionArrays = [quiz.correct_answer, ...quiz.incorrect_answers];
-          optionArrays.sort(() => Math.random() - 0.5);
-          return optionArrays;
-        }),
-      );
+      /*
+      Code 0: Success Returned results successfully.
+      Code 1: No Results Could not return results. The API doesn't have enough questions for your query. (Ex. Asking for 50 Questions in a Category that only has 20.)
+      Code 2: Invalid Parameter Contains an invalid parameter. Arguements passed in aren't valid. (Ex. Amount = Five)
+      Code 3: Token Not Found Session Token does not exist.
+      Code 4: Token Empty Session Token has returned all possible questions for the specified query. Resetting the Token is necessary
+      */
+      const code = data.response_code;
+      if (!code) {
+        setQuizzesArray(data.results);
+        setOptionArray(
+          data.results.map((quiz) => {
+            const optionArrays = [
+              quiz.correct_answer,
+              ...quiz.incorrect_answers,
+            ];
+            optionArrays.sort(() => Math.random() - 0.5);
+            return optionArrays;
+          }),
+        );
+      }
+      if (code === 1) {
+        setTimeout(() => {
+          alert(
+            "No results found for selected options. Redirecting to main page",
+          );
+          setQuizStart(true);
+        }, 5000);
+      }
     }
-  }, [quizStart]);
+  }, [apiUri]);
+
+  useEffect(() => {
+    apiCall();
+  }, [apiUri]);
+
   const quizQuestions = quizzesArray.map((quiz, index) => {
     return (
       <QuizQuestion
@@ -63,6 +91,7 @@ function App() {
     } else if (btnTxt === "Play again") {
       setQuizStart(true);
       setButtonText("Check answers");
+      apiCall();
     }
   }
   return (
--- src/components/ModeSelectionContainer.jsx
+++ src/components/ModeSelectionContainer.jsx
@@ -5,6 +5,35 @@ function ModeSelectionContainer(props) {
   const [isHovered, setIsHovered] = useState(false);
   const btnTxt = props[0];
 
+  function handleClick(e) {
+    setIsHovered(false);
+    const id = e.target.getAttribute("data-id");
+    console.log(id);
+    // For manipulating dicciculty for api call
+    if (id === "difficultyNull") {
+      props.setApiUri({ ...props.apiUri, difficulty: "" });
+    }
+    if (id === "easy" || id === "medium" || id === "hard") {
+      props.setApiUri({ ...props.apiUri, difficulty: `&difficulty=${id}` });
+    }
+
+    // For manipulating type for api call
+    if (id === "typeNull") {
+      props.setApiUri({ ...props.apiUri, type: "" });
+    }
+    if (id === "multiple" || id === "boolean") {
+      props.setApiUri({ ...props.apiUri, type: `&type=${id}` });
+    }
+
+    // For manipulating category for api call
+    if (id === "categoryNull") {
+      props.setApiUri({ ...props.apiUri, category: "" });
+    }
+    if (!isNaN(id)) {
+      props.setApiUri({ ...props.apiUri, category: `&category=${id}` });
+    }
+  }
+
   const modeSelctionItems = Object.values(props[1]).map((modeSelectionItem) => (
     <ModeSelectionItem
       key={modeSelectionItem.id}
@@ -29,7 +58,7 @@ function ModeSelectionContainer(props) {
         <div
           className="mode-selection-items"
           onMouseLeave={() => setIsHovered(false)}
-          onClick={(e) => console.log(e.target.getAttribute("data-id"))}
+          onClick={(e) => handleClick(e)}
         >
           {modeSelctionItems}
         </div>
